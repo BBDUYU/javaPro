84b653668b4b1666a43c2856fc50bffd92c2f1a5
user <user@DESKTOP-AO4C79Q>
2025-08-18 17:25:41 +0900

    변수선언 및 초기화

--- src/days01/Ex04.java
+++ src/days01/Ex04.java
@@ -3,8 +3,12 @@ package days01;
 public class Ex04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String name="오승종";
+			//변수의 초기화
+			//지역 변수는 반드시 초기화를 해야 사용가능
+		String name="오승종"; //변수 선언 & 초기화
+		System.out.println(name);
+
+		name="홍길동"; //초기화
 		System.out.println(name);
 	}
 
---
+++ src/days01/Ex05.java
@@ -0,0 +1,13 @@
+package days01;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		String name1="오승종"; //변수 선언 & 초기화
+		System.out.println(name1);
+
+		String name2="홍길동"; //초기화
+		System.out.println(name2);
+	}
+
+}
