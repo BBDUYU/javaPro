6a71d2e3f4623ae543c7485d59dba3434e855c5b
user <user@DESKTOP-AO4C79Q>
2025-08-19 15:46:57 +0900

    연결연산자

---
+++ src/days02/Ex09_02.java
@@ -0,0 +1,20 @@
+package days02;
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 19. 오후 3:39:39
+ * @subject  
+ * @content + 연산자 설명
+ */
+public class Ex09_02 {
+
+	public static void main(String[] args) {
+		System.out.println(3+5); //덧셈 연산
+		System.out.println("3"+"5"); //연결 연산
+		System.out.println("3"+5); //연결 연산
+		System.out.println("3"+true); //연결 연산
+		System.out.println("3"+0.14); //연결 연산
+		
+	}
+
+}
