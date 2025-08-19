e7d2ff97b6581b32b7c1f77d34a9e04e36434aba
user <user@DESKTOP-AO4C79Q>
2025-08-19 14:11:11 +0900

    boolean

---
+++ src/days02/Ex06.java
@@ -0,0 +1,21 @@
+package days02;
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 19. 오후 2:06:52
+ * @subject 논리타입 
+ * @content 
+ */
+public class Ex06 {
+
+	public static void main(String[] args) {
+		//남 녀 성별 True False
+		boolean gender=true;
+		System.out.println(gender);
+		
+		gender=false;
+		System.out.println(gender);
+		
+	}
+
+}
