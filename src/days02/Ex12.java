c61730b4d04ceaeae57e107823edf60b1efbf24b
user <user@DESKTOP-AO4C79Q>
2025-08-19 16:49:33 +0900

    기본형

---
+++ src/days02/Ex12.java
@@ -0,0 +1,29 @@
+package days02;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		
+		int max=Integer.MAX_VALUE;
+		int min=Integer.MIN_VALUE;
+		
+		System.out.println(max);
+		System.out.println(min);
+		/*
+		 * 	  byte b1;
+		      Byte b2; // Byte 클래스 래퍼(wrapper)클래스
+		
+		      long l1;
+		      Long l2; // Long 클래스
+		
+		      double d1;
+		      Double d2; // Double 클래스
+		
+		      int i1;
+		      Integer i2;
+		
+		      char c1;
+		      Character c2;*/
+	}
+
+}
