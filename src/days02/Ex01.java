4cbbc4256129dac3afed9214b9e6906199e8587e
user <user@DESKTOP-AO4C79Q>
2025-08-19 10:25:38 +0900

    자료형

---
+++ src/days02/Ex01.java
@@ -0,0 +1,31 @@
+package days02;
+
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 19. 오전 10:24:29
+ * @subject 
+ * @content 
+ */
+
+/*
+ * Type ? 
+ * 저장할 값의 종류 (문자,문자열,숫자(정수,실수),논리형등등) +  저장공간크기를 정의한 것을 타입(자료형)이라고 한다.
+	자바의 타입
+    	기본형(Primitive Type) – 8가지
+         	ㄴ 숫자형
+                정수계열 - byte(1), short(2), int(4), long(8)
+                        char(2)
+                        
+                실수계열 – float(4), double(8)
+        	ㄴ 논리형 – true/false          boolean(1)
+    	참조형(Reference Type)  - 배열, 클래스(String), 인터페이스
+ * */
+public class Ex01 {
+
+	public static void main(String[] args) {
+		String first_name, last_name;
+	}
+
+}
+
