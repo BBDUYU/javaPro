73294e8e4d2d19456f9b2a1d556c9a77d4e4d45b
user <user@DESKTOP-AO4C79Q>
2025-08-19 14:46:53 +0900

    형변환

---
+++ src/days02/Ex08.java
@@ -0,0 +1,25 @@
+package days02;
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 19. 오후 2:25:45
+ * @subject 형변환
+ * @content   ㄴ 자동 타입 변환
+ * 			  ㄴ 강제 타입 변환
+ */
+public class Ex08 {
+
+	public static void main(String[] args) {
+//		byte b=50;
+//		long l=b ; -> 큰 타입 = 작은타입 자동 형변환
+		
+//		long l=50;
+//		byte b=(byte)l;
+//		System.out.println(b);
+
+		byte b=10;
+		long l=100;
+		System.out.println(b+l); //작은타입 + 큰타입 => 큰타입
+	}
+
+}
