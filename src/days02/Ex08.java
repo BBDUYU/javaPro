44a38a46b974db89f6ee42ef57e110e273522cbb
user <user@DESKTOP-AO4C79Q>
2025-08-19 14:50:04 +0900

    형변환

--- src/days02/Ex08.java
+++ src/days02/Ex08.java
@@ -6,6 +6,10 @@ package days02;
  * @subject 형변환
  * @content   ㄴ 자동 타입 변환
  * 			  ㄴ 강제 타입 변환
+ * 
+ * 			자동 형 변환
+ * 			  1) byte(작은타입) + int(큰타입) => int(큰타입)
+ * 			  2) long(큰타입) = int(작은타입) 
  */
 public class Ex08 {
 
@@ -17,6 +21,9 @@ public class Ex08 {
 //		byte b=(byte)l;
 //		System.out.println(b);
 
+//		float avg=(float)98.78;
+//		float avg= 98.78f;
+		
 		byte b=10;
 		long l=100;
 		System.out.println(b+l); //작은타입 + 큰타입 => 큰타입
