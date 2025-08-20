c624c8a19d9960f0120f8ad0266e7f04ce3c1781
user <user@DESKTOP-AO4C79Q>
2025-08-20 15:41:13 +0900

    부호연산자

---
+++ src/days03/Ex08_02.java
@@ -0,0 +1,16 @@
+package days03;
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 20. 오후 3:38:48
+ * @subject 부호연산자 
+ * @content 
+ */
+public class Ex08_02 {
+
+	public static void main(String[] args) {
+		System.out.println(+10);
+		System.out.println(-10);
+	}
+
+}
