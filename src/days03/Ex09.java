a075f85ec63372c01a1a8bce8a7ec92eca90048a
BBDUYU <tmdwhddh@gmail.com>
2025-08-20 17:14:15 +0900

    오버/언더플로우

--- src/days03/Ex09.java
+++ src/days03/Ex09.java
@@ -6,9 +6,15 @@ public class Ex09 {
 //		int a = Integer.MAX_VALUE;
 //		a=a+5; //오버플로우
 		
-		int a =Integer.MIN_VALUE;
-		a=a-5; //언더플로우
-		System.out.println(a);
+//		int a =Integer.MIN_VALUE;
+//		a=a-5; //언더플로우
+//		System.out.println(a);
+		
+		int i=Integer.MAX_VALUE;
+		int j=100;
+//		int k=i+j;
+		long k=(long)i+j;
+		System.out.printf("%d + %d=%d",i,j,k);
 	}
 
 }
