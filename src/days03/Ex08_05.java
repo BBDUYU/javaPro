1c3001d81540c7a162dc0a5c3c8c8ff607a5296c
BBDUYU <tmdwhddh@gmail.com>
2025-08-20 16:43:12 +0900

    산술연산자

---
+++ src/days03/Ex08_05.java
@@ -0,0 +1,25 @@
+package days03;
+
+public class Ex08_05 {
+
+	public static void main(String[] args) {
+		System.out.printf("%d+%d=%d\n",10,3,10+3);
+		System.out.printf("%d-%d=%d\n",10,3,10-3);
+		System.out.printf("%d*%d=%d\n",10,3,10*3);
+		System.out.printf("%d/%d=%d\n",10,3,10/3);
+		System.out.printf("%d%%%d=%d\n",10,3,10%3);
+		
+		//System.out.printf("%d/%d=%d\n",10,0,10/0);
+		//System.out.printf("%d%%%d=%d\n",10,0,10%0);
+		// 정수 / || % : Error
+		
+		//Infinity
+		//System.out.printf("%s/%s=%s\n",10,0,10.0/0);
+		
+		//NaN
+		//System.out.printf("%s%%%s=%s\n",10,0,10.0%0);
+
+
+	}
+
+}
