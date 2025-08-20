e91f9f35e17ad20541a45f063141e121092bd594
BBDUYU <tmdwhddh@gmail.com>
2025-08-20 16:27:26 +0900

    후위 전위

---
+++ src/days03/Ex08_03.java
@@ -0,0 +1,13 @@
+package days03;
+
+public class Ex08_03 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		//i +=1;
+		//i++; 후위형
+		//++i; 전위형
+		System.out.printf("> i=%d\n",i);
+	}
+
+}
