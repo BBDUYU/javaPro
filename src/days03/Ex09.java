f5a345e4f7121e2f626ce1d4aaa8683c612ef787
BBDUYU <tmdwhddh@gmail.com>
2025-08-20 16:51:42 +0900

    오버/언더플로우

---
+++ src/days03/Ex09.java
@@ -0,0 +1,14 @@
+package days03;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+//		int a = Integer.MAX_VALUE;
+//		a=a+5; //오버플로우
+		
+		int a =Integer.MIN_VALUE;
+		a=a-5; //언더플로우
+		System.out.println(a);
+	}
+
+}
