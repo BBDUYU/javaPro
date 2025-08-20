b0fb5951a1bdacc58bfd7fe87ebf2dd31ad9232a
BBDUYU <tmdwhddh@gmail.com>
2025-08-20 16:27:47 +0900

    후위형 전위형 계산순서

---
+++ src/days03/Ex08_04.java
@@ -0,0 +1,30 @@
+package days03;
+
+public class Ex08_04 {
+
+	public static void main(String[] args) {
+		int x = 10;
+	      
+	      /* XXX
+	      if (++x > 10) {
+	      //if (x++ > 10) {
+	         System.out.println("크다");
+	      }
+	      
+	      System.out.println("end");
+	      */
+	      
+	      //System.out.printf("> x=%d \n", ++x);  // > x=11 
+	      //System.out.printf("> x=%d \n", x++);  // > x=10 
+	      
+	      //x++;
+	      //++x;
+	      //int y = x++;  // 후위형   > x=11, y=10
+	      //int y = ++x; // 전위형  > x=11, y=11
+	      
+	      //System.out.printf("> x=%d, y=%d\n", x, y);
+
+	   } // main
+	}
+
+
