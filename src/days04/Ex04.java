323d32c6a77bcb49e856990a9ada903723b2eb44
BBDUYU <tmdwhddh@gmail.com>
2025-08-21 11:21:31 +0900

    비트이동연산자

---
+++ src/days04/Ex04.java
@@ -0,0 +1,11 @@
+package days04;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		System.out.println(10<<2);	//0000 1010 -> 0010 1000 -> 40
+		System.out.println(10>>2);  //0000 1010 -> 0000 0010 -> 2
+		System.out.println(10>>>2); //0000 1010 -> 0000 0010 -> 2
+	}
+
+}
