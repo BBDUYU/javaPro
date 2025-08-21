c99892d8c5e181bb13529149700ceb8063c14cbe
BBDUYU <tmdwhddh@gmail.com>
2025-08-21 11:08:25 +0900

    비교연산자

---
+++ src/days04/Ex02.java
@@ -0,0 +1,19 @@
+package days04;
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 21. 오전 10:05:40
+ * @subject 90p 비교연산자
+ * @content 같다	다르다 크다  작다
+ */
+public class Ex02 {
+
+	public static void main(String[] args) {
+		int i =10, j=3;
+		System.out.println(i==j);
+		System.out.println(i!=j);
+		System.out.println(i>=j);
+		System.out.println(i<=j);
+	}
+
+}
