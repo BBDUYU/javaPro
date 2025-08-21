b637f51851da4ec024769bc9ac2e3fbc3e1b7578
BBDUYU <tmdwhddh@gmail.com>
2025-08-21 11:08:00 +0900

    string -> char

---
+++ src/days04/Ex01_02.java
@@ -0,0 +1,14 @@
+package days04;
+
+public class Ex01_02 {
+
+	public static void main(String[] args) {
+		String input="TODO Auto-generated method stub";
+		
+		char firstChar=input.charAt(0);
+		
+		System.out.println(firstChar);
+
+	}
+
+}
