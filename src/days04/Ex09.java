78172c183e9484f5d157868e7db97b7a3c7f0845
BBDUYU <tmdwhddh@gmail.com>
2025-08-21 15:29:06 +0900

    정수비교

---
+++ src/days04/Ex09.java
@@ -0,0 +1,25 @@
+package days04;
+
+import java.util.Scanner;
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 21. 오후 3:11:48
+ * @subject 
+ * @content 
+ */
+public class Ex09 {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b; // 키보드로 부터 정수를 입력      
+		// a,b 정수값이 같은지 여부를 체크 
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("> b 입력 ? ");
+		b = scanner.nextInt();
+
+		System.out.println( a == b ?"같다":"다르다" );
+	}
+
+}
