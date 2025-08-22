b847e2d29290dc32148f28dde230ad224a771236
BBDUYU <tmdwhddh@gmail.com>
2025-08-22 12:41:32 +0900

    for

---
+++ src/days05/Ex03.java
@@ -0,0 +1,18 @@
+package days05;
+
+/**
+ * @author Osj  
+ * @date   2025. 8. 22. 오전 11:30:17
+ * @subject 반복문
+ * @content 동일한 작업을 반복적으로 처리할 때 : 반복문 사용
+ * 반복문: for, foreach
+ */
+public class Ex03 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			System.out.printf("오승종(%d)\n",i);
+		}//for
+	}//main
+
+}//class
---
+++ src/days05/Ex03_02.java
@@ -0,0 +1,17 @@
+package days05;
+
+public class Ex03_02 {
+
+	public static void main(String[] args) {
+//		for(int i=0;i<10;i++) {
+//			System.out.printf("류지호(%d)\n",i);
+//		}
+		int sum=0;
+		for(int i=0;i<10;i++) {
+			System.out.printf("%d+",i);
+			sum+=i;
+		}
+		System.out.printf("=%d",sum);
+	}//main
+
+}//class
