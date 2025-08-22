427218c24d9439dc09834f568e3d123946419394
BBDUYU <tmdwhddh@gmail.com>
2025-08-22 15:06:44 +0900

    for

--- src/days05/Ex05.java
+++ src/days05/Ex05.java
@@ -8,19 +8,43 @@ package days05;
  * 			2) 짝수의 합 출력
  */
 public class Ex05 {
-
 	public static void main(String[] args) {
 		int sum=0;
 		for(int i=1;i<=10;i++) {
-			if(i<=9) System.out.printf("%d+",i);
-			else System.out.printf("%d\n",i);
-			if(i%2==0) { 
-				sum+=i;
-			}//if
-			
+			if(i%2==0)continue; //짝수조건이 될때 continue로 증감식으로 올려버림
+			System.out.printf("%d+",i);
+			sum+=i;
 		}//for
-		System.out.printf("짝수의 합은 %d입니다",sum);
+		System.out.printf("\b=%d",sum);
 
 	}//main
+	//홀수 출력
+//	public static void main(String[] args) {
+//		int sum = 0; 
+//	      for (int i = 1; i <= 10 ; i+=2) {          
+//	            System.out.printf("%d+", i); 
+//	            sum += i;          
+//	      } // for
+//	      System.out.printf("\b=%d\n", sum);
+//
+//	}//main
 
 }//class
+
+//public class Ex05 {
+//
+//	public static void main(String[] args) {
+//		int sum=0;
+//		for(int i=1;i<=10;i++) {
+//			if(i<=9) System.out.printf("%d+",i);
+//			else System.out.printf("%d\n",i);
+//			if(i%2==0) { 
+//				sum+=i;
+//			}//if
+//			
+//		}//for
+//		System.out.printf("짝수의 합은 %d입니다",sum);
+//
+//	}//main
+//
+//}//class
