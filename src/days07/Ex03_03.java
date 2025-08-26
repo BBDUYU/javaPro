28f8f37dd2fccf1dffbbcf275974653645d9383e
BBDUYU <tmdwhddh@gmail.com>
2025-08-26 11:14:31 +0900

    개행

--- src/days07/Ex03_03.java
+++ src/days07/Ex03_03.java
@@ -1,11 +1,11 @@
 package days07;
 
-public class Ex03_02 {
+public class Ex03_03 {
 
 	public static void main(String[] args) {
 		for(int i=0,count=1;i<256;i++,count++) {
 			
-			System.out.printf("[%d] : (%c) ",i,(char)i);
+			System.out.printf("[%c]",(char)i);
 			if(count%10==0) {
 				System.out.println();
 				count=0;
