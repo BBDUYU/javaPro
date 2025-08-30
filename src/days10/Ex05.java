bab61a6233f6c667e0148c4195b275f0e4a39666
BBDUYU <tmdwhddh@gmail.com>
2025-08-30 12:36:04 +0900

    days10

---
+++ src/days10/Ex05.java
@@ -0,0 +1,91 @@
+package days10;
+
+/**
+ * @author kenik  
+ * @date 2025. 8. 29. 오후 5:02:50 
+ * @subject 별찍기  
+ * @content 
+ */ 
+public class Ex05 {
+
+	public static void main(String[] args) {
+		// ****
+		// ****
+		// ****
+		// ****
+		
+		// [1]
+		/*
+		System.out.println("***************");
+		System.out.println("****");
+		System.out.println("****");
+		System.out.println("****");
+		*/
+		
+		// [2]
+		/*
+		for (int i = 1; i <= 4; i++) {
+			System.out.println("****");
+		}
+		*/
+		
+		// [3]
+		for (int i = 1; i <= 4; i++) {
+			//System.out.println("****");
+			for (int j = 1; j <= 4; j++) { // ****
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		/*
+		 
+		
+		  *
+		  **
+		  ***
+		  ****
+		  
+		  ___*
+		  __**
+		  _***
+		  ****
+		  
+		  ****
+		  ***
+		  **
+		  *
+		  
+		  ****
+		  _***
+		  __**
+		  ___*
+		  
+		  
+		  __*__
+		  _***_
+		  *****
+		  
+		  __*__
+		  _***_
+		  *****
+		  _***_
+		  __*__
+		  
+		  
+		  
+		  
+		  
+		 * */
+
+	}
+
+}
+
+
+
+
+
+
+
+
