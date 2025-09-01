e77b621bf04b0c3255ae6dc0fb61f0e8c75107e5
BBDUYU <tmdwhddh@gmail.com>
2025-09-01 11:44:48 +0900

    git

---
+++ src/days11/Ex01.java
@@ -0,0 +1,20 @@
+package days11;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 *
+		 * 	복사 Ctrl + insert
+		 *  붙여넣기 Shift + insert
+			$ git -v
+			git version 2.51.0.windows.1
+			
+			user@DESKTOP-AO4C79Q MINGW64 /c/Class
+			$ git
+
+		 * */
+	}
+
+}
