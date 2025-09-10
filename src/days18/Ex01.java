6bd9ade948b91daad46cf6744b5341c8b4783d13
BBDUYU <tmdwhddh@gmail.com>
2025-09-10 10:03:01 +0900

    다형성

---
+++ src/days18/Ex01.java
@@ -0,0 +1,16 @@
+package days18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		test(al);
+	}
+
+	public static void test(List list) {
+		
+	}
+}
