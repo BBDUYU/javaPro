22f4e17300073b9f8654edf1bb87b1737884dd63
BBDUYU <tmdwhddh@gmail.com>
2025-09-17 10:56:56 +0900

    컬렉션 자료구조

---
+++ src/days23/Ex04.java
@@ -0,0 +1,37 @@
+package days23;
+
+/**
+ * @author Osj  
+ * @date   2025. 9. 17. 오전 10:08:22
+ * @subject Ch 15 - 컬렉션 자료구조 
+ * @content 
+ */
+public class Ex04 {
+
+	public static void main(String[] args) {
+		/*
+		 * JCF
+		 * - Java Collection Framework
+		 * 
+		 * 1) Collection : 데이터(자료) 집합
+		 * 		ㄴ 데이터 효율적 관리(저장, 수정, 삭제 등)하는 객체들
+		 * 			ㄴ 자료구조
+		 * 
+		 * 2) Framework : 표준화 체계(뼈대, 틀)
+		 * 		ㄴ 반복적인 작업을 자동화, 효율성을 높임
+		 * 		   코드의 재사용성 증가 (표준화)
+		 * 
+		 * 3) 자바 언어 컬렉션 프레임워크의 핵심 인터페이스 (3가지)
+		 * 				Collection 인터페이스
+		 * 		
+		 * 		List			Set				Map
+		 * 		-순서유지			-순서유지X			-key와 value 한쌍으로 저장 (entry)
+		 * 		-중복허용			-중복허용X			-key는 중복X value는 중복O
+		 * 
+		 * 	  구현한 컬렉션 클래스
+		 * 	  
+		 * 		
+		 * */
+	}
+
+}
