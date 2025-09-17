b515d082cb634ef181744a8d60e4bfd146e19c38
BBDUYU <tmdwhddh@gmail.com>
2025-09-17 10:56:41 +0900

    @ - annotaion

---
+++ src/days23/Ex03.java
@@ -0,0 +1,29 @@
+package days23;
+
+import java.util.Scanner;
+
+/**
+ * @author Osj  
+ * @date   2025. 9. 17. 오전 9:51:16
+ * @subject   
+ * @content 
+ */
+public class Ex03 {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		String name=scanner.next();
+		
+//		scanner.close();
+		
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
