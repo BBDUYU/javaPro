76cc77c37c0bdb6e043192df181bf1cefd4b6b13
BBDUYU <tmdwhddh@gmail.com>
2025-09-17 10:55:57 +0900

    날짜 형식 -> LocalDate타입

---
+++ src/days23/Ex02_02.java
@@ -0,0 +1,28 @@
+package days23;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Osj  
+ * @date   2025. 9. 17. 오전 9:32:56
+ * @subject  날짜 형식을 띈 String s 문자열 -> LocalDate 타입으로 변환 
+ * 										 dtf.parse -> X
+ * 										 LocalDate.parse(s,dtf) -> O
+ * @content 
+ */
+public class Ex02_02 {
+
+	public static void main(String[] args) {
+		String s = "2025년 02월 17일 (월)";
+		
+		String pattern="yyyy년 MM월 dd일 (E)";
+		
+		DateTimeFormatter dtf=DateTimeFormatter.ofPattern(pattern);
+		
+		//LocalDate d=(LocalDate) dtf.parse(s);
+		LocalDate d = LocalDate.parse(s,dtf);
+		System.out.println(d);
+	}
+
+}
