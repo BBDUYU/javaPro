c65a43101eef240b4b24a1ee78db3dffe6328639
BBDUYU <tmdwhddh@gmail.com>
2025-09-17 10:57:16 +0900

    List 인터페이스

---
+++ src/days23/Ex05.java
@@ -0,0 +1,28 @@
+package days23;
+
+import java.util.ArrayList;
+
+/**
+ * @author Osj  
+ * @date   2025. 9. 17. 오전 10:37:29
+ * @subject List 인터페이스를 구현한 대표적인 컬렉션 클래스 
+ * @content 	ArrayList 컬렉션 클래스
+ * 			특징 - 중복허용, 순서유지
+ */
+public class Ex05 {
+
+	public static void main(String[] args) {
+		//학생 이름은 여러명 저장
+		//배열 사용 시 단점 
+		//	- 배열 크기 자동증가 X
+		//	- 배열 중간에 존재하는 데이터 수정 어려움
+//		String names[]=new String[30];
+		
+		
+		//<E> 제네릭 - Element - 요소
+		ArrayList list=new ArrayList();
+		
+		
+	}
+
+}
