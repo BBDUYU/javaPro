dffd666c037cfedd26cfbfbeb6680d8b3daf6794
BBDUYU <tmdwhddh@gmail.com>
2025-09-19 15:45:10 +0900

    Map

---
+++ src/days25/Ex05.java
@@ -0,0 +1,20 @@
+package days25;
+
+/**
+ * @author Osj  
+ * @date   2025. 9. 19. 오후 2:31:30
+ * @subject 
+ * @content 
+ */
+public class Ex05 {
+	public static void main(String[] args) {
+		/*
+		 * Map
+		 * 	ㄴ HashMap
+		 * 	ㄴ Hashtable
+		 *  ㄴ LinkedHashMap 순서 유지 O, 링크드 리스트 구조(비연결형)
+		 *  ㄴ TreeMap 정렬, 검색, 부분 검색 성능 뛰어남
+		 *  ㄴ Properties 컬렉션 클래스
+		 * */
+	}
+}
