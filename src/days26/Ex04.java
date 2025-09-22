518155bd20a4916aab684bc9e0db684143678444
BBDUYU <tmdwhddh@gmail.com>
2025-09-22 12:31:15 +0900

    IO

---
+++ src/days26/Ex04.java
@@ -0,0 +1,15 @@
+package days26;
+
+/**
+ * @author Osj  
+ * @date   2025. 9. 22. 오후 12:31:09
+ * @subject 
+ * @content 
+ */
+public class Ex04 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
