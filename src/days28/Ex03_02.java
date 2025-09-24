7b3bc1fb51995b514b5955eee233a1f954b7a510
BBDUYU <tmdwhddh@gmail.com>
2025-09-24 15:05:11 +0900

    자식클래스가 직렬화

---
+++ src/days28/Ex03_02.java
@@ -0,0 +1,33 @@
+package days28;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+/**
+ * @author Osj  
+ * @date   2025. 9. 24. 오전 9:39:16
+ * @subject 
+ * @content 
+ */
+public class Ex03_02 {
+
+	public static void main(String[] args) {
+		Child c=null;
+		
+		// 파일에 저장
+		String name=".\\src\\days28\\Child.ser";
+		try (FileInputStream fis=new FileInputStream(name);
+				ObjectInputStream ois=new ObjectInputStream(fis)){
+			c=(Child) ois.readObject();
+			System.out.println(c);
+			// Child [age=20, name=null]
+			// 
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(" end ");
+	}
+
+}
