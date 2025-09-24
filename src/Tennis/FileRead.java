20c32420853d924081f99866352ccbf9ab6ce854
BBDUYU <tmdwhddh@gmail.com>
2025-09-24 15:44:38 +0900

    위치수정

--- src/Tennis/FileRead.java
+++ src/Tennis/FileRead.java
@@ -7,27 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileRead {
-		public static String[][] readPlayerList(String fileName) {
-			List<String> lines = new ArrayList<>();
-	        String line = null;
-	        try (FileReader fr = new FileReader(fileName); 
-	             BufferedReader br = new BufferedReader(fr)) {
-	            int i = 0;
-	            while ((line = br.readLine()) != null) {
-	            	lines.add(line.trim());
-	            }
-	        } catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	        int numRows = (lines.size() + 1) / 2;
-	    	String[][] nameArr = new String[numRows][2];
+	public static String[][] readPlayerList(String fileName) {
+		List<String> lines = new ArrayList<>();//수정용이
+		String line = null;
+		try (FileReader fr = new FileReader(fileName); 
+				BufferedReader br = new BufferedReader(fr)) {
+			while ((line = br.readLine()) != null) {
+				lines.add(line.trim());//공백제거한 br.readLine을 list에 추가
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		int numRows = (lines.size() + 1) / 2; //행 구하는 식
+		String nameArr[][] = new String[numRows][2];//list사이즈에 맞게 행 크기 조정
 
-	    	for (int i = 0; i < lines.size(); i++) {
-	    		nameArr[i / 2][i % 2] = lines.get(i);
-	    	}
-	        return nameArr;
-	    }
+		for (int i = 0; i < lines.size(); i++) {
+			nameArr[i / 2][i % 2] = lines.get(i);//해당배열에 list값 대입
+		}
+		return nameArr;
+	}
 
 }
